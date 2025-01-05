daa4563d6e6aec97ca7dda95b54025992c95cabf
grumblingdev <andrea.conti.dev@gmail.com>
2025-01-05 20:54:15 +0100

    Finished mkdir

--- src/mkdir/mkdir.c
+++ src/mkdir/mkdir.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <libgen.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <bits/posix1_lim.h>
@@ -20,65 +21,55 @@ Create directories if they don't already exist.\n\
 mkdir: missing operand\n\
 Use \"mkdir -h\" for more information.\n"
 
-#define MKDIR_INV_OPT "\
-mkdir: invalid option -- \"%c\"\n"
+#define DEFAULT_MODE (S_IRWXU|S_IRWXG|S_IRWXO)
 typedef int errno_t;
-errno_t mkprnt(const char *path, mode_t mode);
+static errno_t gd_mkdir(const char *path, const mode_t mode, const int p_flag);
 
-int main(int argc, char **argv) {
-    int i, v_flag, p_flag;
-    mode_t mode = (S_IRWXU|S_IRWXG|S_IRWXO);
+int main(int argc, char **argv){
+    int i, p_flag = 0;
+    mode_t mode = DEFAULT_MODE;
 
-    if (argc < 2) {
-        fputs("mkdir: missing operand\n\
-        Use \"mkdir -h\" for more information.\n", stderr);
-        
-        return 1;
-    }
+    if (argc<2){ fputs("mkdir: missing operand\n\
+    Use \"mkdir -h\" for more information.", stderr); return 1; }
     
-    while ((i = getopt(argc, argv, "m:vph") != -1)) {
-        switch (i) {
+    while((i=getopt(argc, argv, "m:ph"))!=-1){
+        switch (i){
             case 'h':
                 puts(MKDIR_HELP_MSG);
                 return 0;
             case 'm':
                 sscanf(optarg, "%o", &mode);
                 break;
-            case 'v':
-                v_flag = 1;
-                break;
             case 'p':
                 p_flag = 1;
                 break;
             case '?':
-                fprintf(stderr, MKDIR_INV_OPT, optopt);
+                return 1;
         }
     }
+    char **p = argv+optind;
+    if(*p==NULL){ fputs(MKDIR_MISSING, stderr); return 1; }
 
-
+    for(;*p;p++)
+        if(gd_mkdir(*p, mode, p_flag) == 1){ fprintf(stderr, "mkdir: unable to create directory \"%s\"\n", *p); return errno; }
     return 0;
 }
 
-errno_t mkprnt(const char *path, mode_t mode) { /* TO IMPROVE */
-    char *token;
-    char *copy = strdup(path);
-    strcpy(copy, path);
-
-    if (!strcmp(copy, "/")) {
-        errno = EEXIST;
-        return -1;
-    } else if (copy[0] == '/' && strcmp(copy, "/"))
-        chdir("/");
-    
-    token = strtok(copy, "/");
-    if (mkdir(token, mode) == -1 && errno != EEXIST) return -1;
-
-    chdir(token);
-    while ((token = strtok(NULL, "/"))) {
-        if (mkdir(token, mode) == -1 && errno != EEXIST) return -1;
-        chdir(token);
-    }
-    chdir(copy);
-    free(copy);
+static errno_t gd_mkdir(const char *path, const mode_t mode, const int p_flag){
+    if(strcmp(path, "/")==0) { errno=EEXIST; return -1; }
+    char *original = getcwd(NULL, 0);
+    if(path[0]=='/') chdir("/");
+    if(p_flag){
+        char *path_copy = strdup(path);
+        char *t;
+        for(t=strtok(path_copy, "/"); t; t=strtok(NULL, "/")){
+            if(mkdir(t, DEFAULT_MODE)!=0 && errno!=EEXIST) { free(path_copy); return 1; }
+            chdir(t);
+        }
+        free(path_copy);
+    }else if(mkdir(path, mode)==-1) return 1;
+    chdir(original);
+    if (chmod(path, mode)!=0) { free(original); return 1; }
+    free(original);
     return 0;
 }
